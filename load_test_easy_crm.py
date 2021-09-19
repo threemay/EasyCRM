63eb5c5888541e6f5355b32d9dab01a560675f5b
threemay <254363807@qq.com>
2021-09-19 17:57:15 +1000

    1

--- load_test_easy_crm.py
+++ load_test_easy_crm.py
@@ -24,35 +24,14 @@ class QuickstartUser(HttpUser):
 
     @events.test_stop.add_listener
     def _(environment, **kw):
-        if environment.stats.total.fail_ratio >= 0:
+        if environment.stats.total.fail_ratio > 0.01:
             logging.error("Test failed due to failure ratio > 1%")
             environment.process_exit_code = 1
-    #     elif environment.stats.total.avg_response_time > 1:
-    #         logging.error("Test failed due to average response time ratio > 200 ms")
-    #         environment.process_exit_code = 1
-    #     elif environment.stats.total.get_response_time_percentile(0.95) > 1:
-    #         logging.error("Test failed due to 95th percentile response time > 800 ms")
-    #         environment.process_exit_code = 1
+        elif environment.stats.total.avg_response_time > 200:
+            logging.error("Test failed due to average response time ratio > 200 ms")
+            environment.process_exit_code = 1
+        elif environment.stats.total.get_response_time_percentile(0.95) > 800:
+            logging.error("Test failed due to 95th percentile response time > 800 ms")
+            environment.process_exit_code = 1
         else:
             environment.process_exit_code = 0
-        print("===========================")
-        print(environment.process_exit_code)
-        print("===========================")
-
-
-import logging
-from locust import events
-
-@events.test_stop.add_listener
-def _(environment, **kw):
-    if environment.stats.total.fail_ratio >= 0:
-        logging.error("Test failed due to failure ratio > 1%")
-        environment.process_exit_code = 1
-    elif environment.stats.total.avg_response_time > 1:
-        logging.error("Test failed due to average response time ratio > 200 ms")
-        environment.process_exit_code = 1
-    elif environment.stats.total.get_response_time_percentile(0.95) > 1:
-        logging.error("Test failed due to 95th percentile response time > 800 ms")
-        environment.process_exit_code = 1
-    else:
-        environment.process_exit_code = 0
